bee2d307c9b253d952b1d4c983d2ca45ea8f289f
Leather Dog Muksihs <muksihs@gmail.com>
2018-06-01 19:03:17 -0400

    skel

--- src/main/java/muksihs/steem/postbrowser/ui/MainView.java
+++ src/main/java/muksihs/steem/postbrowser/ui/MainView.java
@@ -2,10 +2,16 @@ package muksihs.steem.postbrowser.ui;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
 
+import gwt.material.design.client.ui.MaterialPanel;
+
 public class MainView extends Composite {
+	
+	@UiField
+	protected MaterialPanel mainView;
 
 	private static MainViewUiBinder uiBinder = GWT.create(MainViewUiBinder.class);
 
