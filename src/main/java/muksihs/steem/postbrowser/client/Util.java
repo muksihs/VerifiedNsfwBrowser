a1566a098a972f1b07e0d36610cfcc0fee4fd394
Leather Dog Muksihs <muksihs@gmail.com>
2018-06-10 20:01:47 -0400

    fix isSdm check

--- src/main/java/muksihs/steem/postbrowser/client/Util.java
+++ src/main/java/muksihs/steem/postbrowser/client/Util.java
@@ -3,9 +3,10 @@ package muksihs.steem.postbrowser.client;
 import com.google.gwt.core.client.GWT;
 
 public class Util {
-	private static Boolean isSdm;
+	private static Boolean isSdm=null;
 	public static boolean isSdm() {
 		if (isSdm==null) {
+			isSdm=false;
 			GWT.log("isSdm: "+(isSdm=true));
 		}
 		return isSdm;
