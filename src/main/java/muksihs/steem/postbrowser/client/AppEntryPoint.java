57f101265ef25c28973d0d3c17ce4ee38c5e56b0
Leather Dog Muksihs <muksihs@gmail.com>
2018-06-16 01:46:57 -0400

    Try switching to "scriptinjector" to see if it stops SDM issues

--- src/main/java/muksihs/steem/postbrowser/client/AppEntryPoint.java
+++ src/main/java/muksihs/steem/postbrowser/client/AppEntryPoint.java
@@ -1,22 +1,35 @@
 package muksihs.steem.postbrowser.client;
 
+import com.google.gwt.core.client.Callback;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.core.client.GWT.UncaughtExceptionHandler;
+import com.google.gwt.core.client.ScriptInjector;
 
 import muksihs.steem.postbrowser.eventbus.Event;
 import muksihs.steem.postbrowser.eventbus.GlobalAsyncEventBus;
 
 public class AppEntryPoint implements EntryPoint, GlobalAsyncEventBus {
-
+	private static final String STEEMJS="https://cdn.steemjs.com/lib/latest/steem.min.js";
 	@Override
 	public void onModuleLoad() {
 		GWT.setUncaughtExceptionHandler(handler);
-		ViewController.bind();
-		AppController.bind();
-		VerifiedNsfwBlogData.bind();
 		fireEvent((new Event.ShowLoading(true)));
-		fireEvent(new Event.AppStart());
+		Callback<Void, Exception> loaded=new Callback<Void, Exception>() {
+			@Override
+			public void onSuccess(Void result) {
+				ViewController.bind();
+				AppController.bind();
+				VerifiedNsfwBlogData.bind();
+				fireEvent(new Event.AppStart());
+			}
+			
+			@Override
+			public void onFailure(Exception reason) {
+				
+			}
+		};
+		ScriptInjector.fromUrl(STEEMJS).setRemoveTag(false).setWindow(ScriptInjector.TOP_WINDOW).setCallback(loaded).inject();
 	}
 
 	private UncaughtExceptionHandler handler = new UncaughtExceptionHandler() {
