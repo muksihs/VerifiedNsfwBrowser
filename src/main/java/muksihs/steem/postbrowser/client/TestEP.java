0423d602e65f2a578dc3904441815cd2f9668cc5
Leather Dog Muksihs <muksihs@gmail.com>
2018-09-16 16:52:54 -0400

    Add components needed for semi-interactive testing.

---
+++ src/main/java/muksihs/steem/postbrowser/client/TestEP.java
@@ -0,0 +1,11 @@
+package muksihs.steem.postbrowser.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class TestEP implements EntryPoint {
+	@Override
+	public void onModuleLoad() {
+		
+	}
+
+}
